a03950bd62930a5e3a9d156eb0cbc3372d0f7813
Matt Connolly <matt@soundevolution.com.au>
2012-01-17 19:49:49 +1000

    initial commit, new gem from bundler

---
+++ lib/porth-plist.rb
@@ -0,0 +1,7 @@
+require "porth-plist/version"
+
+module Porth
+  module Plist
+    # Your code goes here...
+  end
+end
---
+++ lib/porth-plist/version.rb
@@ -0,0 +1,5 @@
+module Porth
+  module Plist
+    VERSION = "0.0.1"
+  end
+end
